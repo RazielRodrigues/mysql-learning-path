0adcc98c0118c5770bfdee7664ffc5487680fbac
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-06-13 02:06:40 -0300

    joins

--- consultarCidades.sql
+++ consultarCidades.sql
@@ -12,3 +12,36 @@ select
 from estados e
 inner join cidades c
 on e.id = c.estado_id;
+
+select
+ e.nome,
+ c.nome,
+ regiao
+from estados e
+left join cidades c
+on e.id = c.estado_id;
+
+select
+ e.nome,
+ c.nome,
+ regiao
+from estados e
+right join cidades c
+on e.id = c.estado_id;
+
+select
+ e.nome,
+ c.nome,
+ regiao
+from estados e
+left join cidades c
+on e.id = c.estado_id;
+UNION
+-- UNION
+select
+ e.nome,
+ c.nome,
+ regiao
+from estados e
+right join cidades c
+on e.id = c.estado_id;
