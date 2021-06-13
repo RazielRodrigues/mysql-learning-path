e31609462d726cedb8bae379d5f9732520f52dab
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-06-13 01:07:47 -0300

    selects

---
+++ consultarEstados.sql
@@ -0,0 +1,3 @@
+select * from estados;
+select nome as 'Nome estados', sigla from estados where regiao = 'Sul';
+select nome, regiao, populacao from estados where populacao >= 10 order by populacao desc;
